4d1307979ecc3431109106ec251e2af4ffb21c6c
itc_user <milacukierman@gmail.com>
2016-06-30 16:20:10 +0300

    coding

--- js_MemoryGame.js
+++ js_MemoryGame.js
@@ -1,3 +1,71 @@
 /**
  * Created by itc_user on 6/30/2016.
  */
+
+var genericalCard  = null;
+
+//MAKING THE CARDS
+
+var imgArray = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg"];
+imgArray = shuffle(imgArray);
+function boardCreate() {
+
+    var board = document.getElementById('board');
+    var count = 0;
+    for (var i = 0; i < 3; i++) {
+
+        var row = document.createElement('div');
+        row.className = "line";
+        board.appendChild(row);
+
+        for (var j = 0; j < 4; j++) {
+            var card = document.createElement('div');
+            card.className = "card";
+            var img = document.createElement('img');
+            img.src = "./img/" + imgArray[count];
+            card.appendChild(img);
+           card.addEventListener("click", clickCard);
+            row.appendChild(card);
+            count++;
+        }
+    }
+}
+
+boardCreate();
+
+// RANDOMIZING THE CARDS
+
+function shuffle(imgArray) {
+    for (var i = imgArray.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = imgArray[i];
+        imgArray[i] = imgArray[j];
+        imgArray[j] = temp;
+    }
+    return imgArray;
+}
+
+//EVENT TO SHOW THE CARD
+
+function clickCard(event) {
+    var clickedCard = event.target;
+    clickedCard.getElementsByTagName("img")[0].style.display = "inline";
+    if (genericalCard == null){
+        genericalCard = clickedCard;
+    }
+    else{
+        if (genericalCard.getElementsByTagName("img")[0].src == clickedCard.getElementsByTagName("img")[0].src){
+            genericalCard = null;
+        }else{
+            setTimeout(function(){
+                clickedCard.getElementsByTagName("img")[0].style.display = "none";
+                genericalCard.getElementsByTagName("img")[0].style.display = "none";
+                genericalCard = null;
+            },1000);
+        }
+    }
+
+
+
+}
+
